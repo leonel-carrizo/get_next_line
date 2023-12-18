a64243066d40919a2e940b875b431116c5cc977b
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2023-12-18 12:18:27 +0000

    backup all

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
+/*                                                       :::       ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 00:58:48 by lcarrizo          #+#    #+#             */
-/*   Updated: 2023/12/13 04:33:10 by lcarrizo          ###   ##london.com     */
+/*   Updated: 2023/12/15 21:19:16 by lcarrizo          ###   ##london.com     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,42 @@
 
 char	*get_next_line(int fd)
 {
-	size_t		BUFFER_SIZE;
 	char		*buff;
 	ssize_t		n_bytes;
-	char 		*line;
+	char		*line;
 
-	if (fd == -1)
-	{
-		perror("Error");
-		return ("nada");
-	}
-	BUFFER_SIZE = 10;
+	if (fd < 0 || BUFFER_SIZE <=0)
+		return (NULL);
 	buff = (char *)malloc(BUFFER_SIZE);
 	if (!buff)
 		return (NULL);
 	line = buff;
-	while ((n_bytes = read(fd, buff, BUFFER_SIZE)) > 0)
+	n_bytes = read(fd, buff, BUFFER_SIZE);
+	while (n_bytes > 0)
 		write(1, buff, n_bytes);
-	if(n_bytes == 0)
-		return ("menos");
+	if (n_bytes < 0)
+	{
+		free(buff);
+		return (NULL);
+	}
 	return (line);
 }
 
 int	main(void)
 {
-	int	fd;
-	const char	*path;
+	int		fd;
 	char	*line;
-	
-	path = "/home/l3o/42-London/get_next_line/get_next_line-trash/tests/test.txt";
-	fd = open(path, O_RDONLY);
-	// loop for calling  get)next_line()
-	line =	get_next_line(fd);
-	printf("The text is: %s\n", line);
 
-	close(fd);
+	fd = open("tests/test", O_RDONLY);
+	while (fd == 1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s\n", line);
+		free(line);
+	}
+	// close(fd);
 	return (0);
 }
 
@@ -69,36 +70,41 @@ int	main(void)
 // Repetir el hasta el llegar al final del archivo.
 // Cerrar el archivo.
 //
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 // app.diagrams.net
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* Write a function that returns a line read from a file descriptor:
  * fd: The file descriptor to read from
  * Read line: correct behavior
  * NULL: there is nothing else to read, or an error occurred.
- * ----------------------------------------------------------------------------
+ * -----------------------------------------------------------------------------
  * - Repeated calls (e.g., using a loop) to your get_next_line() function 
  *  should let you read the text file pointed to by the file descriptor, 
  *  one line at a time. 
  * - Your function should return the line that was read.
- * - If there is nothing else to read or if an error occurred, it should return NULL.
+ * - If there is nothing else to read or if an error occurred, 
+ *   it should return NULL.
  * - Function must works as expected both when reading a file and 
  *  when reading from the standard input.
- * - Please note that the returned line should include the terminating \n character,
+ * - Please note that the returned line should include the terminating \n 
+ *   character,
  *  except if the end of file was reached and does not end with a \n character.
  * - add this option to your: compiler call: -D BUFFER_SIZE=n 
  *  It will define the buffer size for read(). 
- *  The buffer size value will be modified by your peer-evaluators and the Moulinette 
- *  in order to test your code. (cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 <files>.c)
+ *  The buffer size value will be modified by your peer-evaluators and 
+ *  the Moulinette 
+ *  in order to test your code. 
+ *  (cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 <files>.c)
  * - An undefined behavior when reading a binary file. 
- *  However, you can implement a logical way to handle this behavior if you want to.
- * - An undefined behavior if the file pointed to by the file descriptor changed 
+ *  However, you can implement a logical way to handle this behavior 
+ *  if you want to.
+ * - An undefined behavior if the file pointed to by 
+ *   the file descriptor changed 
  *  since the last call whereas read() didn’t reach the end of file.
  * - Try to read as little as possible each time get_next_line() is called. 
  *  If you encounter a new line, you have to return the current line. 
  *  Don’t read the whole file and then process each line.
  * - You are not allowed to use your libft in this project.
  * - lseek() is forbidden.
- * - Global variables are forbidden.
+ *- Global variables are forbidden.
  */
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lcarrizo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 21:04:37 by lcarrizo          #+#    #+#             */
-/*   Updated: 2023/12/11 17:17:04 by lcarrizo         ###   ########.fr       */
+/*   Updated: 2023/12/16 02:02:09 by lcarrizo          ###   ##london.com     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,24 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 // Just to test: (DELETE WHEN FINISH)
 # include <limits.h>
 # include <string.h>
 # include <stdio.h>
 
+//to define nodo list
+typedef struct		s_list
+{
+	char		*str_readós;
+	struct s_list	*next;
+}			t_list;
+
 //to define predefined BUFFER SIZE 
-//...
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 //Functions prototypes
 char	*get_next_line(int fd);
